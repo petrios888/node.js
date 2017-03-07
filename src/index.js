4f2ede863b00ac0ab4d6142c5396f3fca1d175c0
petrios888 <appeter22@yahoo.com>
2017-03-07 02:30:26 -0500

    Beta.3

--- src/index.js
+++ src/index.js
@@ -1,25 +1,43 @@
-import React from 'react'
+import React, { Component} from 'react'
 import ReactDOM from 'react-dom';
-
-
+import YTsearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_lists';
+//import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyD0sM9yiDqADYwDquuRBkBKzS6wIzyhV7M';
 
 
 
-// Create New Component
-// some HTML
-
-// youtube API key = ####################################
-
-const API_KEY = '#######################################';
 
 
-const App =  () => {     return (<div> <SearchBar/> </div>) ;  }
+class  App extends Component {
+	constructor(props) {
+	super(props);
 
+	this.state = { videos:[]  };
+	YTsearch({key: API_KEY, term:'surfboards'}, (videos) => {
+		this.setState({ videos });
+	});
+	}
+
+
+
+
+
+
+	render (){
+
+
+	return   (
+		<div>
+		<SearchBar/>
+		<VideoList videos = {this.state.videos}/>
+		</div>  )
+	}
+}
 
 
 
 
 ReactDOM.render(< App /> , document.querySelector('.container'));
-
-
