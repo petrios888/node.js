0cc4779e06b66038798e02da32466e6f0c9bc4fd
petrios888 <appeter22@yahoo.com>
2017-03-11 01:42:06 -0500

    Delta commit

---
+++ book_stuff/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function (){
+  return [
+    { title: 'Javascript: The Good  Parts'},
+    { title: ' Coding in Python'},
+    { title: ' Coding in PHP'},
+    { title: ' Simply C++'}
+  ];
+}
