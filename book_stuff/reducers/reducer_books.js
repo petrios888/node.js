f8f126d2c53e61ff10b66c1480bb4c2f0e71c0fe
petrios888 <appeter22@yahoo.com>
2017-03-11 20:28:08 -0500

    Delta.3

--- book_stuff/reducers/reducer_books.js
+++ book_stuff/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function (){
   return [
-    { title: 'Javascript: The Good  Parts'},
-    { title: ' Coding in Python'},
-    { title: ' Coding in PHP'},
-    { title: ' Simply C++'}
+    { title: 'Javascript: The Good  Parts', pages: 101},
+    { title: ' Coding in Python', pages: 1600},
+    { title: ' Coding in PHP', pages: 888},
+    { title: ' Simply C++', pages:100}
   ];
 }
